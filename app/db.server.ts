306dfe41624b74bca5ca9c7ecf12c3203c31b0d5
DaCurse <dacurse0@gmail.com>
2022-04-21 15:36:39 +0300

    Refactor for consistent export style

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,16 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
+export let prisma: PrismaClient
 
 declare global {
   // eslint-disable-next-line no-var
   var __db__: PrismaClient
 }
 
-// this is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-// in production we'll have a single connection to the DB.
+// this is needed because in development we don't want to restart the server
+// with every change, but we want to make sure we don't create a new connection
+// to the DB with every change either. in production we'll have a single
+// connection to the DB.
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
@@ -20,5 +20,3 @@ if (process.env.NODE_ENV === 'production') {
   prisma = global.__db__
   prisma.$connect()
 }
-
-export { prisma }
