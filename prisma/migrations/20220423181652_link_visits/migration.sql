ceeea65cbfe54ab9839d7e2150f654a29d92f83b
DaCurse <dacurse0@gmail.com>
2022-04-23 21:39:02 +0300

    Rename `LinkVisit` model to `Visit`

--- prisma/migrations/20220423181652_link_visits/migration.sql
+++ prisma/migrations/20220423181652_link_visits/migration.sql
@@ -8,15 +8,15 @@
 ALTER TABLE "Link" DROP COLUMN "visits";
 
 -- CreateTable
-CREATE TABLE "LinkVisit" (
+CREATE TABLE "Visit" (
     "id" TEXT NOT NULL,
     "linkId" TEXT NOT NULL,
     "visitedAt" TIMESTAMP(3) NOT NULL,
     "ipAddress" VARCHAR(15) NOT NULL,
     "userAgent" TEXT NOT NULL,
 
-    CONSTRAINT "LinkVisit_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Visit_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "LinkVisit" ADD CONSTRAINT "LinkVisit_linkId_fkey" FOREIGN KEY ("linkId") REFERENCES "Link"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Visit" ADD CONSTRAINT "Visit_linkId_fkey" FOREIGN KEY ("linkId") REFERENCES "Link"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
